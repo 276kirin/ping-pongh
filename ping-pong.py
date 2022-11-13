e8ee051d8a58093683b3d20d0823284e7fe66f78
276kirin <117577743+276kirin@users.noreply.github.com>
2022-11-13 17:05:43 +0300

    Update ping-pong.py

--- ping-pong.py
+++ ping-pong.py
@@ -2,9 +2,13 @@ from pygame import *
 from random import randint
 
 class GameSprite(sprite.Sprite):
-    def __init__(self,sp_image,sp_speed,sp_x,sp_y):
+    def __init__(self,sp_speed,sp_x,sp_y,width,height,sp_image=None):
         super().__init__()
-        self.image = transform.scale(image.load(sp_image),(65,65))
+        if sp_image == None:
+            self.image = Surface((width,height))
+            self.image.fill((0,0,0))
+        else:
+            self.image = transform.scale(image.load(sp_image),(width,height))
         self.speed = sp_speed
 
         self.rect = self.image.get_rect()
@@ -14,28 +18,64 @@ class GameSprite(sprite.Sprite):
     def reset(self):
         window.blit(self.image,(self.rect.x,self.rect.y))
 
-class Player(GameSprite): # какой нибудь комментарий
+class Rocket(GameSprite):
+    def __init__(self,sp_speed,sp_x,sp_y,width,height,color,):
+        super().__init__(sp_speed,sp_x,sp_y,width,height)
+        self.fill_color = color
+        self.image.fill(color)
+
+    def reset(self):
+        window.blit(self.image, (self.rect.x,self.rect.y))
 
     def update_l(self):
         keys = key.get_pressed()
-        if keys[K_UP] and self.rect.x > 5:
+        if keys[K_w] and self.rect.y > 5:
             self.rect.y -= self.speed
-        if keys[K_DOWN] and self.rect.x < win_w - 80:
+        if keys[K_s] and self.rect.y < win_h - 80:
             self.rect.y += self.speed
 
     def update_r(self):
         keys = key.get_pressed()
-        if keys[K_W] and self.rect.x > 5:
+        if keys[K_UP] and self.rect.y > 5:
             self.rect.y -= self.speed
-        if keys[K_S] and self.rect.x < win_w - 80:
+        if keys[K_DOWN] and self.rect.y < win_h - 80:
             self.rect.y += self.speed
 
-win_w = 400
-win_h = 300
-
+# палитра
+rocket1_color = (0,0,255)
+rocket2_color = (255,0,0)
+back = (200,255,255)
+# создание фона и окна
+win_w = 600
+win_h = 500
 window = display.set_mode((win_w,win_h))
 display.set_caption('ping-pong')
+window.fill(back)
+# создание ракеток и мяча
+rocket1 = Rocket(10,0,0,10,125,rocket1_color)
+rocket2 = Rocket(10,win_w-10,0,10,125,rocket2_color)
+ball = GameSprite(4,200,200,50,50,'ball.png')
 
-while True:
+game = True
+finish = False
+clock = time.Clock()
+FPS = 60
+
+
+while game:
+    for e in event.get():
+        if e.type == QUIT:
+            game = False
+    if finish != True:
+        window.fill(back)
+        rocket1.update_l()
+        rocket2.update_r()
+        ball.rect.x += ball.speed
+        ball.rect.y += ball.speed
+
+        rocket1.reset()
+        rocket2.reset()
+        ball.reset()
+        
     display.update()
     time.delay(50)
